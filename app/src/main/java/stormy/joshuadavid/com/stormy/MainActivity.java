647342b10a89241f90702379ff0b124793421842
JoshuaDavid <jshuadvd@icloud.com>
2015-04-27 18:28:55 -0700

    Added Variables for the API KEY + Forecast Url + Latitude + Longitude

--- app/src/main/java/stormy/joshuadavid/com/stormy/MainActivity.java
+++ app/src/main/java/stormy/joshuadavid/com/stormy/MainActivity.java
@@ -2,8 +2,7 @@ package stormy.joshuadavid.com.stormy;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+
 
 
 public class MainActivity extends ActionBarActivity {
@@ -14,26 +13,12 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
+    String apiKey = "d4582e37a190726ee8741a4f27a81e3c";
+    double latitude = 37.8267;
+    double longitude = -122.423;
+    String forecastUrl = "https://api.forecast.io/forecast/" + apiKey + "/" + latitude +  "," + longitude;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
-        return super.onOptionsItemSelected(item);
-    }
 }
