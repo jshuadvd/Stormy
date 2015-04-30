50e24a25d88221f848c3c31dc021d81f850750cb
JoshuaDavid <jshuadvd@icloud.com>
2015-04-30 15:08:01 -0700

    Added code for isAvailable

--- app/src/main/java/stormy/joshuadavid/com/stormy/MainActivity.java
+++ app/src/main/java/stormy/joshuadavid/com/stormy/MainActivity.java
@@ -1,7 +1,9 @@
 package stormy.joshuadavid.com.stormy;
 
 import android.app.FragmentManager;
+import android.content.Context;
 import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.nfc.Tag;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -74,7 +76,19 @@ public class MainActivity extends ActionBarActivity {
 
     private boolean isNetworkAvailable() {
 
-        ConnectivityManager manager = getSystemService();
+        ConnectivityManager manager = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        // Instantiate a Network Info Object
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        boolean isAvailable = false;
+        if (networkInfo != null && networkInfo.isConnected()) {
+
+            isAvailable = true;
+
+        }
+
+        return isAvailable;
 
     }
 
