f0c9a9d45fc569107fcd7f4ec125611cbc3afdb0
JoshuaDavid <jshuadvd@icloud.com>
2015-05-01 22:03:54 -0700

    Created member variables to correspond with the data to display

--- app/src/main/java/stormy/joshuadavid/com/stormy/CurrentWeather.java
+++ app/src/main/java/stormy/joshuadavid/com/stormy/CurrentWeather.java
@@ -4,4 +4,12 @@ package stormy.joshuadavid.com.stormy;
  * Created by joshuadavid on 5/1/15.
  */
 public class CurrentWeather {
+
+    private String mIcon;
+    private long mTime;
+    private double mTemperature;
+    private double mHumidity;
+    private double mPrecipChance;
+    private String mSummary;
+    
 }
