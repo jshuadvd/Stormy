21a6017632da5d1ef77a044db69e2731bf36e4f9
JoshuaDavid <jshuadvd@icloud.com>
2015-05-09 19:51:34 -0700

    Created Forecast Class to bundle all of the Data Classes

---
+++ app/src/main/java/stormy/joshuadavid/com/stormy/Forecast.java
@@ -0,0 +1,12 @@
+package stormy.joshuadavid.com.stormy;
+
+/**
+ * Created by joshuadavid on 5/9/15.
+ */
+public class Forecast {
+
+    private CurrentWeather mCurrentWeather;
+    private Hour[] mHourlyForecast;
+    private Day[] mDailyForecast;
+    
+}
