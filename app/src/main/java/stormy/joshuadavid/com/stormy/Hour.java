4266a07a999639c7dd574fe6c5df04f5b639431f
JoshuaDavid <jshuadvd@icloud.com>
2015-05-09 19:43:15 -0700

    Added Getters & Setters for Hour class

--- app/src/main/java/stormy/joshuadavid/com/stormy/Hour.java
+++ app/src/main/java/stormy/joshuadavid/com/stormy/Hour.java
@@ -8,8 +8,47 @@ public class Hour {
     private String mSummary;
     private double mTemperature;
     private String mIcon;
-    private String mTimeZone;
-    
+    private String mTimezone;
 
 
+    // Getters & Setters
+    public long getTime() {
+        return mTime;
+    }
+
+    public void setTime(long time) {
+        mTime = time;
+    }
+
+    public String getSummary() {
+        return mSummary;
+    }
+
+    public void setSummary(String summary) {
+        mSummary = summary;
+    }
+
+    public double getTemperature() {
+        return mTemperature;
+    }
+
+    public void setTemperature(double temperature) {
+        mTemperature = temperature;
+    }
+
+    public String getIcon() {
+        return mIcon;
+    }
+
+    public void setIcon(String icon) {
+        mIcon = icon;
+    }
+
+    public String getTimezone() {
+        return mTimezone;
+    }
+
+    public void setTimezone(String timezone) {
+        mTimezone = timezone;
+    }
 }
