5849b2369ef620408f8136123d904c28c52ef60d
JoshuaDavid <jshuadvd@icloud.com>
2015-05-09 19:41:40 -0700

    Added hour class to hold data for each hour along with an array of hour items to hold the forecast information

---
+++ app/src/main/java/stormy/joshuadavid/com/stormy/Hour.java
@@ -0,0 +1,15 @@
+package stormy.joshuadavid.com.stormy;
+
+/**
+ * Created by joshuadavid on 5/9/15.
+ */
+public class Hour {
+    private long mTime;
+    private String mSummary;
+    private double mTemperature;
+    private String mIcon;
+    private String mTimeZone;
+    
+
+
+}
