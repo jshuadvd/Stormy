f9547546079906cd80103f08b80df84bf2f3aaa7
JoshuaDavid <jshuadvd@icloud.com>
2015-05-13 20:36:17 -0700

    Setting this adapter as the adapter for the listView

--- app/src/main/java/stormy/joshuadavid/com/stormy/ui/DailyForecastActivity.java
+++ app/src/main/java/stormy/joshuadavid/com/stormy/ui/DailyForecastActivity.java
@@ -16,10 +16,16 @@ public class DailyForecastActivity extends ListActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_forecast);
 
+        // The data to pass
         String[] daysOfTheWeek = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+
+        // Adapter that syas how the data should be laid out
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1,
                 daysOfTheWeek);
+
+        // Setting this adapter as the adapter for the listView
+        setListAdapter(adapter);
     }
 
 
