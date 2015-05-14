133ccd392a78644d4213da228e30286316f44a95
JoshuaDavid <jshuadvd@icloud.com>
2015-05-13 20:33:54 -0700

    Finished Adapter that says how the data should be laid out

--- app/src/main/java/stormy/joshuadavid/com/stormy/ui/DailyForecastActivity.java
+++ app/src/main/java/stormy/joshuadavid/com/stormy/ui/DailyForecastActivity.java
@@ -17,7 +17,9 @@ public class DailyForecastActivity extends ListActivity {
         setContentView(R.layout.activity_daily_forecast);
 
         String[] daysOfTheWeek = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
-        ArrayAdapter<String> adapter;
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                daysOfTheWeek);
     }
 
 
