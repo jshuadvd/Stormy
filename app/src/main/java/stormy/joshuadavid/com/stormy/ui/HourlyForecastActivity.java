19b1f1bd44ce74abf443f6a4205d12ea89987494
JoshuaDavid <jshuadvd@icloud.com>
2015-05-18 19:27:44 -0700

    Adding the Hour Parcelable Intent

--- app/src/main/java/stormy/joshuadavid/com/stormy/ui/HourlyForecastActivity.java
+++ app/src/main/java/stormy/joshuadavid/com/stormy/ui/HourlyForecastActivity.java
@@ -1,9 +1,13 @@
 package stormy.joshuadavid.com.stormy.ui;
 
+import android.content.Intent;
+import android.os.Parcelable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.util.Arrays;
 
 import stormy.joshuadavid.com.stormy.R;
 import stormy.joshuadavid.com.stormy.weather.Hour;
@@ -16,6 +20,11 @@ public class HourlyForecastActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hourly_forecast);
+
+        Intent intent = getIntent();
+        Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.HOURLY_FORECAST);
+        mHours = Arrays.copyOf(parcelables, parcelables.length, Hour[].class);
+
     }
 
 }
