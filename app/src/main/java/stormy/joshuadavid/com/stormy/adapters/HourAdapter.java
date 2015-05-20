8446a140698511146f655d41b3671fe21d848328
JoshuaDavid <jshuadvd@icloud.com>
2015-05-19 22:55:28 -0700

    Adding HourViewHolder

--- app/src/main/java/stormy/joshuadavid/com/stormy/adapters/HourAdapter.java
+++ app/src/main/java/stormy/joshuadavid/com/stormy/adapters/HourAdapter.java
@@ -3,6 +3,10 @@ package stormy.joshuadavid.com.stormy.adapters;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import stormy.joshuadavid.com.stormy.R;
 
 /**
  * Created by joshuadavid on 5/19/15.
@@ -26,8 +30,18 @@ public class HourAdapter extends RecyclerView.Adapter {
 
     public class HourViewHolder extends RecyclerView.ViewHolder<HourViewHolder> {
 
+        public TextView mTimeLabel;
+        public TextView mSummaryLabel;
+        public TextView mTemperatureLabel;
+        public ImageView mIconImageView;
+
         public HourViewHolder(View itemView) {
             super(itemView);
+
+            mTimeLabel (TextView) itemView.findViewById(R.id.timeLabel);
+            mSummaryLabel (TextView) itemView.findViewById(R.id.summaryLabel);
+            mTemperatureLabel (TextView) itemView.findViewById(R.id.temperatureLabel);
+            mIconImageView (ImageView) itemView.findViewById((R.id.iconImageView));
         }
     }
 
