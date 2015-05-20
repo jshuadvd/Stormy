8447111ae94968f6ba9f6c26708b5c01fb1e1108
JoshuaDavid <jshuadvd@icloud.com>
2015-05-19 22:40:55 -0700

    Adding Hour Adapter

---
+++ app/src/main/java/stormy/joshuadavid/com/stormy/adapters/HourAdapter.java
@@ -0,0 +1,12 @@
+package stormy.joshuadavid.com.stormy.adapters;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by joshuadavid on 5/19/15.
+ */
+public class HourAdapter extends RecyclerView.Adapter {
+
+    
+
+}
