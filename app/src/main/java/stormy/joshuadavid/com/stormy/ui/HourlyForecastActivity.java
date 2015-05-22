18366e5615614de0dc5f37bf14a9e86a55f83f96
JoshuaDavid <jshuadvd@icloud.com>
2015-05-21 21:45:01 -0700

    Creating Hourly Adapter

--- app/src/main/java/stormy/joshuadavid/com/stormy/ui/HourlyForecastActivity.java
+++ app/src/main/java/stormy/joshuadavid/com/stormy/ui/HourlyForecastActivity.java
@@ -4,11 +4,14 @@ import android.content.Intent;
 import android.os.Parcelable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import java.util.Arrays;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 import stormy.joshuadavid.com.stormy.R;
 import stormy.joshuadavid.com.stormy.weather.Hour;
 
@@ -16,10 +19,13 @@ public class HourlyForecastActivity extends ActionBarActivity {
 
     private Hour[] mHours;
 
+    @InjectView(R.id.recyclerView) RecyclerView mRecyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hourly_forecast);
+        ButterKnife.inject(this);
 
         Intent intent = getIntent();
         Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.HOURLY_FORECAST);
